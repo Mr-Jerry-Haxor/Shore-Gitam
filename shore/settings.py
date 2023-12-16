801749174b38feedd3aec2619e0f1d423e4a20b2
githubaccount-2022 <shravanjerry2001@gmail.com>
2023-12-16 16:46:04 +0530

    postgresql

--- shore/settings.py
+++ shore/settings.py
@@ -84,13 +84,24 @@ WSGI_APPLICATION = 'shore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME' : 'shoredb',
+        'USER' : 'shorepostgres',
+        'PASSWORD' : 'shore@123$',
+        'HOST' : 'localhost',
+        'PORT' : 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
