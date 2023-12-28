3ccde80bbe8d4b46d9881e1fb2c7b8a11741ff1a
githubaccount-2022 <shravanjerry2001@gmail.com>
2023-12-28 09:23:29 +0530

    Update admin.py

--- promotion/admin.py
+++ promotion/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 from django.contrib import admin
 from .models import BGMIPlayer
 
 @admin.register(BGMIPlayer)
-class BGMIPlayerAdmin(admin.ModelAdmin):
+class BGMIPlayerAdmin(ImportExportModelAdmin):
     list_display = ('name', 'userid', 'email', 'regno', 'yearofstudy', 'campus', 'created_at')
     search_fields = ('name', 'userid', 'email', 'regno', 'campus')
     list_filter = ('yearofstudy', 'campus')
