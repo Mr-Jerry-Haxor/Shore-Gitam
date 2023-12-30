eed3e437d126d7cd8a9effe1aab35a2186019981
githubaccount-2022 <shravanjerry2001@gmail.com>
2023-12-30 12:31:45 +0530

    Update models.py

--- promotion/models.py
+++ promotion/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class BGMIPlayer(models.Model):
     name = models.CharField(max_length=100)
     userid = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=False)
     regno = models.CharField(max_length=100)
     yearofstudy = models.CharField(max_length=100)
     campus = models.CharField(max_length=100)
