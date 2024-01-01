d51c02f9490288103909155987bbb1ee08d372ad
githubaccount-2022 <shravanjerry2001@gmail.com>
2024-01-01 14:50:20 +0530

    payments model deleted

--- payments/admin.py
+++ payments/admin.py
@@ -10,9 +10,9 @@ class FestPassAdmin(ImportExportModelAdmin):
     list_filter = ('payment_status', 'payment_date')
     date_hierarchy = 'payment_date'
 
-@admin.register(Registrations)
-class RegistrationsAdmin(ImportExportModelAdmin):
-    list_display = ('name', 'email', 'payment_status', 'payment_date', 'payment_method')
-    search_fields = ('name', 'email', 'registration_number')
-    list_filter = ('payment_status', 'payment_date')
-    date_hierarchy = 'payment_date'
+# @admin.register(Registrations)
+# class RegistrationsAdmin(ImportExportModelAdmin):
+#     list_display = ('name', 'email', 'payment_status', 'payment_date', 'payment_method')
+#     search_fields = ('name', 'email', 'registration_number')
+#     list_filter = ('payment_status', 'payment_date')
+#     date_hierarchy = 'payment_date'
