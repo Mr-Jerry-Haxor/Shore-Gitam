82769add0fade4921d57db9a974d1536004c20f9
githubaccount-2022 <shravanjerry2001@gmail.com>
2024-01-01 15:02:33 +0530

    payments

--- payments/admin.py
+++ payments/admin.py
@@ -1,18 +1,11 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
-# Register your models here.
-from .models import FestPass
+from .models import FestPass, Registrations
 
 @admin.register(FestPass)
 class FestPassAdmin(ImportExportModelAdmin):
-    list_display = ('name', 'email', 'payment_status', 'payment_date', 'payment_method')
-    search_fields = ('name', 'email', 'registration_number')
-    list_filter = ('payment_status', 'payment_date')
-    date_hierarchy = 'payment_date'
+    list_display = ['name', 'mobile', 'email', 'gender', 'posted_date', 'updated_date']
 
-# @admin.register(Registrations)
-# class RegistrationsAdmin(ImportExportModelAdmin):
-#     list_display = ('name', 'email', 'payment_status', 'payment_date', 'payment_method')
-#     search_fields = ('name', 'email', 'registration_number')
-#     list_filter = ('payment_status', 'payment_date')
-#     date_hierarchy = 'payment_date'
+@admin.register(Registrations)
+class RegistrationsAdmin(ImportExportModelAdmin):
+    list_display = ['name', 'mobile', 'email', 'gender', 'posted_date', 'updated_date']
