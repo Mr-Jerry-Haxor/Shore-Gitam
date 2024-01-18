7930cbabb776737d1682b1834b9ff11ec2169ca5
githubaccount-2022 <shravanjerry2001@gmail.com>
2024-01-18 08:22:20 +0530

    Update admin.py

--- payments/admin.py
+++ payments/admin.py
@@ -6,14 +6,14 @@ from .models import FestPass, Registrations
 class FestPassAdmin(ImportExportModelAdmin):
     list_display = ['name', 'mobile', 'email', 'gender', 'posted_date', 'amount' , 'transaction_status' , 'confirm_id']
     list_filter = ['transaction_status']
-    list_search = ['name', 'mobile', 'email', 'gender', 'posted_date', 'amount' , 'transaction_status' , 'confirm_id' , 'order_id']
+    search_fields = ['name', 'mobile', 'email',  'posted_date', 'txn_id' , 'confirm_id' , 'order_id']
     date_hierarchy = 'posted_date'
 
 @admin.register(Registrations)
 class RegistrationsAdmin(ImportExportModelAdmin):
     list_display = ['name', 'mobile', 'email', 'gender', 'posted_date', 'amount' ,'transaction_status' , 'confirm_id']
     list_filter = ['transaction_status']
-    list_search = ['name', 'mobile', 'email', 'gender', 'posted_date', 'amount' , 'transaction_status' , 'confirm_id' , 'order_id']
+    search_fields = ['name', 'mobile', 'email', 'posted_date' , 'txn_id' , 'confirm_id' , 'order_id']
     date_hierarchy = 'posted_date'
 
 from import_export import resources
