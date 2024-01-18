3cbc750638b84c8c714c7fb9cd3cff68886b29f8
SHRAVAN JERIPOTHULA <98485154+Mr-Jerry-Haxor@users.noreply.github.com>
2024-01-18 10:20:11 +0000

    basic design of models

--- security/models.py
+++ security/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# Create your models for log entry and exit for the students
+
+"""
+
+tables:
+
+security staff : email ; (boolean :  main gate , fest , open audi )
+
+main gate entries / status
+
+fest entries / status
+
+open audi entries / status
+
+
+
+
+
+class security_staff(models.Model):
+    email_id = 
+
+"""
