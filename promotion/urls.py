4297049d2832322ce981e6c243c80f9f99797071
SHRAVAN JERIPOTHULA <98485154+Mr-Jerry-Haxor@users.noreply.github.com>
2024-01-19 08:47:21 +0000

    noc url

--- promotion/urls.py
+++ promotion/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('bgmi', views.bgmi_player, name='bgmireg'),
     path('volunteer' , views.volunteer_registration, name='volunteer'),
-    path('noc_and_tickets', views.noc_and_travel_tickets , name='noc_and_tickets'),
+    path('noc_and_tickets_test', views.noc_and_travel_tickets , name='noc_and_tickets'),
 ]
