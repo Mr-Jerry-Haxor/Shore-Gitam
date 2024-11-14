0b670d3901b67bb16fe71346530bc6b9c398721f
AkshitUriti <akshituriti@intendcareer.com>
2024-11-14 12:11:38 +0530

    changes in static settings

--- shore/settings.py
+++ shore/settings.py
@@ -145,9 +145,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
