66789967e3a398312a62fc1ebb53f03f786c12cc
AkshitUriti <akshituriti@intendcareer.com>
2024-11-16 17:58:46 +0530

    minor

--- shore/settings.py
+++ shore/settings.py
@@ -147,9 +147,10 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
