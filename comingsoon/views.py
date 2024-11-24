d66313bb76db1507435498f03c9121fb32512fc7
SHRAVAN JERIPOTHULA <shravanjerry2001@gmail.com>
2024-11-25 01:15:59 +0530

    Update views.py

--- comingsoon/views.py
+++ comingsoon/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render , HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
-
 
 def test(request):
     return render(request, 'countdown.html')
     # return render(request, 'SHORE24_home.html')
 
 def index(request):
-    return render(request, 'SHORE24_home.html')
+    return redirect('homepage')
