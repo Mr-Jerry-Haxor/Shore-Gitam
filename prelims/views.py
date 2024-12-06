dff45eb24732941b73973824d93c710587cd7994
Sai Akshit Uriti <akshit.uriti@gmail.com>
2024-12-06 20:09:16 +0530

    added email validation

--- prelims/views.py
+++ prelims/views.py
@@ -137,6 +137,7 @@ def register(request, event_name):
             if not (
                 captain_email.endswith("@gitam.in")
                 or captain_email.endswith("@gitam.edu")
+                or captain_email.endswith("@student.gitam.edu")
             ):
                 raise ValidationError("Invalid Email Domain")
         except ValidationError:
@@ -201,19 +202,19 @@ def register(request, event_name):
                                 request, "prelims/prelims_registration.html", context
                             )
 
-                        # try:
-                        #     validate_email(email)
-                        #     if not (
-                        #         email.endswith("@gitam.in")
-                        #         or email.endswith("@gitam.edu")
-                        #         or email.endswith("@student.gitam.edu")
-                        #     ):
-                        #         raise ValidationError("Invalid Email Domain")
-                        # except ValidationError:
-                        #     messages.error(request, "Invalid email")
-                        #     return render(
-                        #         request, "prelims/prelims_registration.html", context
-                        #     )
+                        try:
+                            validate_email(email)
+                            if not (
+                                email.endswith("@gitam.in")
+                                or email.endswith("@gitam.edu")
+                                or email.endswith("@student.gitam.edu")
+                            ):
+                                raise ValidationError("Invalid Email Domain")
+                        except ValidationError:
+                            messages.error(request, "Invalid email")
+                            return render(
+                                request, "prelims/prelims_registration.html", context
+                            )
 
                         if len(phone) != 10 or not phone.isdigit():
                             messages.error(request, "Invalid Phone Number")
