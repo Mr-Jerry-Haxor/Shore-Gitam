6db1210cb501bfff8e21c408be2564d6420c4c62
Sai Akshit Uriti <akshit.uriti@gmail.com>
2024-12-22 09:50:49 +0530

    modified trigger

--- payments/signals.py
+++ payments/signals.py
@@ -6,35 +6,20 @@ from home.views import send_email_async, send_festpass_email, send_payment_faile
 
 @receiver(post_save, sender=FestPass)
 def after_save_trigger(sender, instance, created, **kwargs):
-    if created:
-        if instance.transaction_status == "Y":
-            # send email mentioning successful pass purchase
-            send_email_async(instance.email, send_festpass_email)
-        elif instance.transaction_status == "N":
-            # send email mentioning failed pass purchase
-            send_email_async(instance.email, send_payment_failed_email)
-    else:
-        if instance.transaction_status == "Y":
-            # send email mentioning successful pass purchase
-            send_email_async(instance.email, send_festpass_email)
-        elif instance.transaction_status == "N":
-            # send email mentioning failed pass purchase
-            send_email_async(instance.email, send_payment_failed_email)
+    # if instance is created
+    if instance.transaction_status == "Y":
+        # send email mentioning successful pass purchase
+        send_email_async(instance.email, send_festpass_email)
+    elif instance.transaction_status == "N":
+        # send email mentioning failed pass purchase
+        send_email_async(instance.email, send_payment_failed_email)
 
 
 @receiver(post_save, sender=Registrations)
 def after_save_trigger1(sender, instance, created, **kwargs):
-    if created:
-        if instance.transaction_status == "Y":
-            # send email mentioning successful pass purchase
-            send_email_async(instance.email, send_festpass_email)
-        elif instance.transaction_status == "N":
-            # send email mentioning failed pass purchase
-            send_payment_failed_email(instance.email, send_festpass_email)
-    else:
-        if instance.transaction_status == "Y":
-            # send email mentioning successful pass purchase
-            send_email_async(instance.email, send_festpass_email)
-        elif instance.transaction_status == "N":
-            # send email mentioning failed pass purchase
-            send_payment_failed_email(instance.email, send_festpass_email)
+    if instance.transaction_status == "Y":
+        # send email mentioning successful pass purchase
+        send_email_async(instance.email, send_festpass_email)
+    elif instance.transaction_status == "N":
+        # send email mentioning failed pass purchase
+        send_payment_failed_email(instance.email, send_festpass_email)
