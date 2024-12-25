a5081de48b5855bb0e90d55a91b491533fbfd414
Sai Akshit Uriti <akshit.uriti@gmail.com>
2024-12-25 15:25:12 +0530

    modifed forgot password

--- forgot_password/urls.py
+++ forgot_password/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 from .views import *
 
 urlpatterns = [
     path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
     path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_done/', TemplateView.as_view(template_name="forgot_password/password_reset_done.html"), name='password_reset_done'),
+    path('password_reset_complete/', TemplateView.as_view(template_name="forgot_password/password_reset_complete.html"), name='password_reset_complete'),
 ]
