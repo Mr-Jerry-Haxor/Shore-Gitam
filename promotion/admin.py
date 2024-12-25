2d7288eeac980fa520c39ea0dfc8ed9b2b23f545
Sai Akshit Uriti <akshit.uriti@gmail.com>
2024-12-25 20:17:28 +0530

    modified volunteer

--- promotion/admin.py
+++ promotion/admin.py
@@ -6,7 +6,7 @@ from .models import Volunteer
 class VolunteerAdmin(ImportExportModelAdmin):
     list_display = ['name', 'email', 'year_of_study', 'created_at', 'isvolunteer']
     search_fields = ['name', 'email', 'institute', 'year_of_study', 'regno']
-    list_filter = ('Campus', 'domain_of_interest', 'ishosteler', 'isvolunteer')
+    list_filter = ('ishosteler', 'isvolunteer')
     
     # Optional: Add ordering
     ordering = ('-created_at',)
--- promotion/views.py
+++ promotion/views.py
@@ -10,6 +10,24 @@ from events.models import College, Event, Hackathon
 from hospitality.models import HospitalityUser
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
+from home.views import send_email_async
+from django.conf import settings
+from django.template.loader import get_template
+from django.core.mail import EmailMultiAlternatives
+from django.http import HttpResponse
+
+
+def send_success_volunteer_registration(user_email):
+    user = Volunteer.objects.get(email=user_email)
+
+    subject = "Shore'25 || Successfully volunteer registration"
+    from_email = settings.EMAIL_HOST_USER
+    html_content = get_template("volunteer_success_email.html").render({"user": user})
+
+    msg = EmailMultiAlternatives(subject, html_content, from_email, [user_email])
+    msg.content_subtype = "html"
+    msg.send()
+
 
 @require_http_methods(["GET", "POST"])
 def bgmi_player(request):
@@ -58,7 +76,6 @@ def bgmi_player(request):
 @login_required(login_url="/auth/login/google-oauth2/")
 def volunteer_registration(request):
     if request.method == "POST":
-        print(f"\n{request.POST}\n")
         try:
             name = request.POST.get("name")
             email = request.POST.get("email")
@@ -77,13 +94,6 @@ def volunteer_registration(request):
             if email != request.user.email:
                 messages.error(request, "You can only register using your email.")
                 return redirect("volunteer")
-
-            # Validate email domain
-            # if not re.match(r".+@(?:gitam\.edu|gitam\.in|student\.gitam\.edu)$", email):
-            #     messages.error(
-            #         request, "Invalid email domain. Please use a Gitam email."
-            #     )
-            #     return redirect("volunteer")
 
             # Validate image file type and size
             if isinstance(profile_pic, InMemoryUploadedFile):
@@ -118,6 +128,9 @@ def volunteer_registration(request):
             )
             volunteer.save()  # Save the data to the database
             messages.success(request, "Successfully completed volunteer registration.")
+
+            send_email_async(email, send_success_volunteer_registration)
+
             return render(request, "volunteer_success.html")
         except IntegrityError as e:
             print(e)
@@ -132,6 +145,14 @@ def volunteer_registration(request):
     else:
         return render(request, "volunteer_registration.html")
 
+
+def send_volunteer_emails(request):
+    if request.user.is_superuser:
+        volunteers = Volunteer.objects.all()
+        for volunteer in volunteers:
+            send_email_async(volunteer.email, send_success_volunteer_registration)
+        return HttpResponse("Done, sent emails!!")
+    return redirect("home:homepage")
 
 def noc(request):
     return redirect("hospitality:hospitalitynoc")
