fb3604c22a9bf91849ef24c7dfd6f4c8f709c880
Sai Akshit Uriti <akshit.uriti@gmail.com>
2024-12-26 11:24:47 +0530

    modifed email

--- shore/settings.py
+++ shore/settings.py
@@ -26,6 +26,8 @@ if DEVELOPMENT:
         }
     }
 
+    # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
     EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
     EMAIL_FILE_PATH = BASE_DIR / "emails"
 else:
