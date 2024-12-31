1dd6b7c96662070462157cb42568f27299db7839
Sai Akshit Uriti <akshit.uriti@gmail.com>
2024-12-31 09:03:36 +0530

    modified admin

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
-from .models import PaymentIssueEmail
+from .models import PaymentIssueEmail, FreePass
 
-admin.site.register(PaymentIssueEmail)
+@admin.register(FreePass)
+class FreePassAdmin(admin.ModelAdmin):
+    list_display = ('email', 'created_at')
+    search_fields = ('email',)
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at',)
+
+@admin.register(PaymentIssueEmail)
+class PaymentIssueEmailAdmin(admin.ModelAdmin):
+    list_display = ('user', 'payment', 'status', 'added_time')
+    search_fields = ('user__email', 'payment__email', 'status')
+    list_filter = ('status', 'added_time')
+    ordering = ('-added_time',)
+    readonly_fields = ('added_time',)
