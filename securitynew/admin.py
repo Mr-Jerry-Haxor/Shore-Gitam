97cd97eb924a7814d7678eafacc39a713fe433e5
Sai Akshit Uriti <akshit.uriti@gmail.com>
2025-01-04 09:20:16 +0530

    added import export to userin model

--- securitynew/admin.py
+++ securitynew/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import UserIn
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+class UserInResource(resources.ModelResource):
+    class Meta:
+        model = UserIn
+        fields = ('user', 'in_time', 'is_user_in', 'email')
 
 @admin.register(UserIn)
-class UserInAdmin(admin.ModelAdmin):
+class UserInAdmin(ImportExportModelAdmin):
+    resource_class = UserInResource
     list_display = ('user', 'in_time', 'is_user_in', 'email')
     list_filter = ('is_user_in', 'in_time')
     search_fields = ('user__name', 'user__email')  # Assuming email is a field in CustomUser model
